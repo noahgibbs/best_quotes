8aa3fb856fd948c252d206db8ba66bdd75c4563e
Noah Gibbs <noah_gibbs@yahoo.com>
2012-12-04 20:27:11 -0800

    Add a real view

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,5 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "There is nothing either good or bad " +
-      "but thinking makes it so."
+    render :a_quote, :noun => :winking
   end
 end
