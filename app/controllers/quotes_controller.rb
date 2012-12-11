64a78ace83c5d2327ff56ec36ca5754453ce308b
Noah Gibbs <noah_gibbs@yahoo.com>
2012-12-10 22:01:48 -0800

    Add object creation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,19 @@ class QuotesController < Rulers::Controller
     quote_1 = FileModel.find(1)
     render :quote, :obj => quote_1
   end
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels.",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
 end
