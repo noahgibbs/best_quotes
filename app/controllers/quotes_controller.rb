b56c07783743bad8b1e6028f9391ba948782f322
Noah Gibbs <noah_gibbs@yahoo.com>
2012-12-10 21:46:27 -0800

    Add file models

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,9 @@ class QuotesController < Rulers::Controller
   def a_quote
     render :a_quote, :noun => :winking
   end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
 end
