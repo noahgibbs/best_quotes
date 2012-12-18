cce5e6257fc33283045c7de6cb3200e98b0ff64e
Noah Gibbs <noah_gibbs@yahoo.com>
2012-12-18 03:31:04 -0800

    User render_response in show action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,7 +16,7 @@ class QuotesController < Rulers::Controller
   def show
     quote = FileModel.find(params["id"])
     ua = request.user_agent
-    render :quote, :obj => quote, :ua => ua
+    render_response :quote, :obj => quote, :ua => ua
   end
 
   def new_quote
